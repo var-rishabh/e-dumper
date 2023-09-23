9f06ec27df499b5cc87269a8218693c8b788f5ca
Rishabh Varshney <65810355+var-rishabh@users.noreply.github.com>
2023-09-23 20:14:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     define: {
-    __APP_ENV__: import.meta.env.VITE_VERCEL_ENV,
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
   },
 })
