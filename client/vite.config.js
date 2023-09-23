62fd73c23d8c37511fd65273e4f85ee2aef2fee9
Rishabh Varshney <right.rishabh@gmail.com>
2023-09-24 01:15:48 +0530

    v1.2 🚀

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(() => {
   return {
     plugins: [react()],
     define: {
-      __APP_ENV__: JSON.stringify(env.VITE_VERCEL_ENV),
+      "process.env": JSON.stringify(process.env),
     },
   }
 })
